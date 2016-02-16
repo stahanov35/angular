8ccaba35f0fd7289bc66f8d6d802f20e506d6f40
denis.kolesnik <denis.kolesnik@dev-pro.net>
2016-02-16 02:16:52 +0200

    Property search page - Init/Error state

--- app/app.js
+++ app/app.js
@@ -9,15 +9,27 @@
 var propCrossApp = angular.module('propCrossApp', []);
 
 // Declare controller for search result
-propCrossApp.controller('SearchData', ['$scope', function ($scope) {
-	$scope.property = [
-		{
-	        name: 'Ghost caste',
-	        count: 12
-    	},
-    	{
-	        name: 'Name caste',
-	        count: 8
-    	}
-   ]
-}])
+propCrossApp.controller('SearchData', function ($scope, $http) {
+	var baseUrl 			= 'http://api.nestoria.co.uk/api',
+			jsonCallback 	= '&callback=JSON_CALLBACK';
+
+	$scope.getLocationData = function () {
+		console.log($scope.place);
+		if(!angular.isUndefined($scope.place)){
+			$http.jsonp(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + $scope.place + jsonCallback).
+				success(function(data){
+					$scope.data = data;
+					var response = $scope.data.response;
+
+					$scope.locations 			= response.locations;
+					$scope.totalResults 	= response.total_results;
+					$scope.boolStatusCode = (parseInt(response.application_response_code) < 112) ? false : true;
+
+					console.log($scope.data, $scope.boolStatusCode);
+				}).
+				error(function (data) {
+					$scope.data = "Request failed";
+				});
+		}
+	}
+})
