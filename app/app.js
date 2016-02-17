7b2452cb98549b22d37333c90f68f17308ed0d38
denis.kolesnik <denis.kolesnik@dev-pro.net>
2016-02-18 01:06:30 +0200

    Rewrite JS code in Google code style || Add my location with fixed place

--- app/app.js
+++ app/app.js
@@ -1,35 +1,41 @@
 'use strict'
 
-// Declare app level module which depends
 /**
 * propCrossApp Module
-*
-* Description
 */
 var propCrossApp = angular.module('propCrossApp', []);
 
-// Declare controller for search result
+/**
+* Declare controller for search result
+*/
 propCrossApp.controller('SearchData', function ($scope, $http) {
 	var baseUrl 			= 'http://api.nestoria.co.uk/api',
 			jsonCallback 	= '&callback=JSON_CALLBACK';
 
-	$scope.getLocationData = function () {
-		console.log($scope.place);
-		if(!angular.isUndefined($scope.place)){
-			$http.jsonp(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + $scope.place + jsonCallback).
-				success(function(data){
-					$scope.data = data;
-					var response = $scope.data.response;
+	$scope.getLocationData = function ($event) {
+		var elTarget = angular.element($event.target),
+				response;
 
-					$scope.locations 			= response.locations;
-					$scope.totalResults 	= response.total_results;
-					$scope.boolStatusCode = (parseInt(response.application_response_code) < 112) ? false : true;
+		if (elTarget.attr('data-my-location')){
+				$scope.place = elTarget.attr('data-my-location');
+		}
 
-					console.log($scope.data, $scope.boolStatusCode);
-				}).
-				error(function (data) {
-					$scope.data = "Request failed";
-				});
+		if (!angular.isUndefined($scope.place)) {
+			$http.jsonp(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + 
+									$scope.place + 
+									jsonCallback).
+					success(function(data){
+						$scope.data = data;
+						response 	= $scope.data.response;
+						$scope.locations = response.locations;
+						$scope.totalResults = response.total_results;
+						$scope.boolStatusCode = (parseInt(response.application_response_code) < 112) ? false : true;
+					}).
+					error(function (data) {
+						$scope.data = 'Request failed';
+					});
+		} else {
+			$scope.boolStatusCode = true;
 		}
 	}
 })
