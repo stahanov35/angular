49c377730206d867dd3b35615121c25e30569f23
denis.kolesnik <denis.kolesnik@dev-pro.net>
2016-03-03 23:20:14 +0200

    Configurte routes

--- app/app.js
+++ app/app.js
@@ -1,74 +1,20 @@
 'use strict'
 
+var dependences = [
+	'ngRoute'
+];
 /**
 * propCrossApp Module
 */
-var propCrossApp 					= angular.module('propCrossApp', ['ngRoute', 'propCrossControllers']);
-var propCrossControllers 	= angular.module('propCrossControllers', []);
+var propCrossApp = angular.module('propCrossApp', dependences);
 
-propCrossApp.config(function ($routeProvider) {
-	$routeProvider.when('/search', {
-		templateUrl: 'search/search.tpl.html'
-	});
-	$routeProvider.when('/main', {
-		templateUrl: 'main/main.tpl.html'
-	});
-	$routeProvider.otherwise({ redirectTo: '/' });
-});
-
-/**
-* Declare controller for search result
-*/
-propCrossControllers.controller('SearchData', ['$scope', '$http', function ($scope, $http) {
-	$scope.stepPages 		= 3;
-	var baseUrl 				= 'http://api.nestoria.co.uk/api',
-			jsonCallback 		= '&callback=JSON_CALLBACK';
-
-	$scope.getLocationData = function (myLocation, pageNumber) {
-		var response,
-				page = pageNumber || 1;
-
-		$scope.place = myLocation;
-
-		if (!angular.isUndefined($scope.place)) {
-			console.log(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + 
-									$scope.place + 
-									jsonCallback +
-									'&page=' + pageNumber);
-			$http.jsonp(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + 
-									$scope.place + 
-									jsonCallback +
-									'&page=' + pageNumber).
-					success(function(data){
-						$scope.data 					= data;
-						response 							= $scope.data.response;
-						$scope.locations 			= response.locations;
-						$scope.totalResults 	= response.total_results;
-						$scope.listings 			= response.listings;
-						$scope.searchError 		= (parseInt(response.application_response_code) < 212) ? false : true;
-						console.log(response);
-					}).
-					error(function (data) {
-						$scope.data 					= 'Request failed';
-					});
-		} else {
-			$scope.searchError = true;
-		}
-	}
-
-	$scope.showResult = function () {
-		$scope.bollShowResult = true;
-	}
-
-	// Pagination
-	// @TODO: Rewrite as component
-	$scope.prev = function () {
-		
-	}
-	$scope.next = function () {
-		
-	}
-	$scope.getTimes=function(n){
-	  return new Array(n);
-	};
-}])
+propCrossApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/main', {
+      templateUrl: '/views/main.tpl.html',
+      controller: 'SearchData'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}]);
---
+++ app/js/controllers.js
@@ -0,0 +1,58 @@
+'use strict'
+
+/**
+* Declare controller for search result
+*/
+propCrossApp.controller('SearchData', ['$scope', '$http', function ($scope, $http) {
+	$scope.stepPages 		= 3;
+	var baseUrl 				= 'http://api.nestoria.co.uk/api',
+			jsonCallback 		= '&callback=JSON_CALLBACK';
+
+	$scope.getLocationData = function (myLocation, pageNumber) {
+		var response,
+				page = pageNumber || 1;
+
+		$scope.place = myLocation;
+
+		if (!angular.isUndefined($scope.place)) {
+			console.log(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + 
+									$scope.place + 
+									jsonCallback +
+									'&page=' + pageNumber);
+			$http.jsonp(baseUrl + '?action=search_listings&country=uk&encoding=json&listing_type=buy&place_name=' + 
+									$scope.place + 
+									jsonCallback +
+									'&page=' + pageNumber).
+					success(function(data){
+						$scope.data 					= data;
+						response 							= $scope.data.response;
+						$scope.locations 			= response.locations;
+						$scope.totalResults 	= response.total_results;
+						$scope.listings 			= response.listings;
+						$scope.searchError 		= (parseInt(response.application_response_code) < 212) ? false : true;
+						console.log(response);
+					}).
+					error(function (data) {
+						$scope.data 					= 'Request failed';
+					});
+		} else {
+			$scope.searchError = true;
+		}
+	}
+
+	$scope.showResult = function () {
+		$scope.bollShowResult = true;
+	}
+
+	// Pagination
+	// @TODO: Rewrite as component
+	$scope.prev = function () {
+		
+	}
+	$scope.next = function () {
+		
+	}
+	$scope.getTimes=function(n){
+	  return new Array(n);
+	};
+}])
